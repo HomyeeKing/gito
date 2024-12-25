dd8cfc38bb1d929da330b018705afad9b9c7cc0d
HomyeeKing <HomyeeKing@gmail.com>
2024-12-25 20:13:51 +0800

    fix: get current branch

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -17,7 +17,7 @@ pub fn get_git_info() -> GitInfo {
   let ssh_url = get_stdout(&run_git(vec!["config", "remote.origin.url"])).unwrap();
   let username = get_stdout(&run_git(vec!["config", "user.name"])).unwrap();
   let email = get_stdout(&run_git(vec!["config", "user.email"])).unwrap();
-  let current_branch = get_stdout(&run_git(vec!["rev-parse", "--abbrev-ref", "HEAD"])).unwrap();
+  let current_branch = get_stdout(&run_git(vec!["branch", "--show-current"])).unwrap();
 
   GitInfo {
     user_repo: get_user_repo(&ssh_url),
